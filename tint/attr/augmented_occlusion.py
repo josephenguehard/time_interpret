ab99c84f5c68a8fb4adfc3d6764f1c08470068ad
josephenguehard <josephenguehard@gmail.com>
2022-07-22 17:19:48 +0100

    Add docs

--- tint/attr/augmented_occlusion.py
+++ tint/attr/augmented_occlusion.py
@@ -13,6 +13,16 @@ from typing import Any, Callable, Tuple, Union
 
 
 class AugmentedOcclusion(Occlusion):
+    """
+    Augmented Occlusion by sampling the baseline from a bootstrapped
+    distribution.
+
+    Args:
+        forward_func (callable): The forward function of the model or
+            any modification of it
+        data (tuple, Tensor): The data from which the baselines are sampled.
+    """
+
     def __init__(
         self,
         forward_func: Callable,
@@ -36,6 +46,118 @@ class AugmentedOcclusion(Occlusion):
         perturbations_per_eval: int = 1,
         show_progress: bool = False,
     ) -> TensorOrTupleOfTensorsGeneric:
+        """
+
+        Args:
+            inputs (tensor or tuple of tensors):  Input for which occlusion
+                    attributions are computed. If forward_func takes a single
+                    tensor as input, a single input tensor should be provided.
+                    If forward_func takes multiple tensors as input, a tuple
+                    of the input tensors should be provided. It is assumed
+                    that for all given input tensors, dimension 0 corresponds
+                    to the number of examples (aka batch size), and if
+                    multiple input tensors are provided, the examples must
+                    be aligned appropriately.
+            sliding_window_shapes (tuple or tuple of tuples): Shape of patch
+                    (hyperrectangle) to occlude each input. For a single
+                    input tensor, this must be a tuple of length equal to the
+                    number of dimensions of the input tensor - 1, defining
+                    the dimensions of the patch. If the input tensor is 1-d,
+                    this should be an empty tuple. For multiple input tensors,
+                    this must be a tuple containing one tuple for each input
+                    tensor defining the dimensions of the patch for that
+                    input tensor, as described for the single tensor case.
+            strides (int or tuple or tuple of ints or tuple of tuples, optional):
+                    This defines the step by which the occlusion hyperrectangle
+                    should be shifted by in each direction for each iteration.
+                    For a single tensor input, this can be either a single
+                    integer, which is used as the step size in each direction,
+                    or a tuple of integers matching the number of dimensions
+                    in the occlusion shape, defining the step size in the
+                    corresponding dimension. For multiple tensor inputs, this
+                    can be either a tuple of integers, one for each input
+                    tensor (used for all dimensions of the corresponding
+                    tensor), or a tuple of tuples, providing the stride per
+                    dimension for each tensor.
+                    To ensure that all inputs are covered by at least one
+                    sliding window, the stride for any dimension must be
+                    <= the corresponding sliding window dimension if the
+                    sliding window dimension is less than the input
+                    dimension.
+                    If None is provided, a stride of 1 is used for each
+                    dimension of each input tensor.
+                    Default: None
+            target (int, tuple, tensor or list, optional):  Output indices for
+                    which difference is computed (for classification cases,
+                    this is usually the target class).
+                    If the network returns a scalar value per example,
+                    no target index is necessary.
+                    For general 2D outputs, targets can be either:
+
+                    - a single integer or a tensor containing a single
+                      integer, which is applied to all input examples
+
+                    - a list of integers or a 1D tensor, with length matching
+                      the number of examples in inputs (dim 0). Each integer
+                      is applied as the target for the corresponding example.
+
+                    For outputs with > 2 dimensions, targets can be either:
+
+                    - A single tuple, which contains #output_dims - 1
+                      elements. This target index is applied to all examples.
+
+                    - A list of tuples with length equal to the number of
+                      examples in inputs (dim 0), and each tuple containing
+                      #output_dims - 1 elements. Each tuple is applied as the
+                      target for the corresponding example.
+                    Default: None
+            additional_forward_args (any, optional): If the forward function
+                    requires additional arguments other than the inputs for
+                    which attributions should not be computed, this argument
+                    can be provided. It must be either a single additional
+                    argument of a Tensor or arbitrary (non-tuple) type or a
+                    tuple containing multiple additional arguments including
+                    tensors or any arbitrary python types. These arguments
+                    are provided to forward_func in order following the
+                    arguments in inputs.
+                    For a tensor, the first dimension of the tensor must
+                    correspond to the number of examples. For all other types,
+                    the given argument is used for all forward evaluations.
+                    Note that attributions are not computed with respect
+                    to these arguments.
+                    Default: None
+            perturbations_per_eval (int, optional): Allows multiple occlusions
+                    to be included in one batch (one call to forward_fn).
+                    By default, perturbations_per_eval is 1, so each occlusion
+                    is processed individually.
+                    Each forward pass will contain a maximum of
+                    perturbations_per_eval * #examples samples.
+                    For DataParallel models, each batch is split among the
+                    available devices, so evaluations on each available
+                    device contain at most
+                    (perturbations_per_eval * #examples) / num_devices
+                    samples.
+                    Default: 1
+            show_progress (bool, optional): Displays the progress of
+                    computation. It will try to use tqdm if available for
+                    advanced features (e.g. time estimation). Otherwise, it
+                    will fallback to a simple output of progress.
+                    Default: False
+
+        Returns:
+            *tensor* or tuple of *tensors* of **attributions**:
+                - **attributions** (*tensor* or tuple of *tensors*):
+                    The attributions with respect to each input feature.
+                    Attributions will always be
+                    the same size as the provided inputs, with each value
+                    providing the attribution of the corresponding input index.
+                    If a single tensor is provided as inputs, a single tensor
+                    is returned. If a tuple is provided for inputs, a tuple of
+                    corresponding sized tensors is returned.
+        """
+        # Change input to tuple and check that its length is the same as data.
+        # Also check that each dimension between inputs and self.data matches
+        # except on the first one.
         inputs = _format_input(inputs)
         _validate_input(
             inputs=inputs,
