8cd7f6c0004c09c4aab60e4e517db3da915cab8e
josephenguehard <josephenguehard@gmail.com>
2022-07-29 15:13:25 +0100

    Refactor timed ig

--- tint/attr/timed_ig.py
+++ tint/attr/timed_ig.py
@@ -4,7 +4,10 @@ import typing
 from captum.attr import IntegratedGradients
 from captum.log import log_usage
 from captum._utils.common import (
+    _expand_additional_forward_args,
+    _expand_target,
     _format_input,
+    _format_additional_forward_args,
     _is_tuple,
     _format_tensor_into_tuples,
     _format_output,
@@ -15,9 +18,11 @@ from captum._utils.typing import (
     TargetType,
     TensorOrTupleOfTensorsGeneric,
 )
+from captum.attr._utils.approximation_methods import approximation_parameters
+from captum.attr._utils.common import _reshape_and_sum
 
 from torch import Tensor
-from typing import Any, Callable, Tuple, Union, cast
+from typing import Any, Callable, List, Tuple, Union, cast
 
 
 class TemporalIntegratedGradients(IntegratedGradients):
@@ -132,6 +137,117 @@ class TemporalIntegratedGradients(IntegratedGradients):
             return_convergence_delta,
             delta,
         )
+
+    def _attribute(
+        self,
+        inputs: Tuple[Tensor, ...],
+        baselines: Tuple[Union[Tensor, int, float], ...],
+        target: TargetType = None,
+        additional_forward_args: Any = None,
+        n_steps: int = 50,
+        method: str = "gausslegendre",
+        step_sizes_and_alphas: Union[
+            None, Tuple[List[float], List[float]]
+        ] = None,
+    ) -> Tuple[Tensor, ...]:
+        if step_sizes_and_alphas is None:
+            # retrieve step size and scaling factor for specified
+            # approximation method
+            step_sizes_func, alphas_func = approximation_parameters(method)
+            step_sizes, alphas = step_sizes_func(n_steps), alphas_func(n_steps)
+        else:
+            step_sizes, alphas = step_sizes_and_alphas
+
+        # scale features and compute gradients.
+        # (batch size is abbreviated as bsz)
+        # scaled_features' dim -> (bsz * #steps x inputs[0].shape[1:], ...)
+        scaled_features_tpl = self.scale_features(
+            inputs=inputs,
+            baselines=baselines,
+            alphas=alphas,
+        )
+
+        additional_forward_args = _format_additional_forward_args(
+            additional_forward_args
+        )
+        # apply number of steps to additional forward args
+        # currently, number of steps is applied only to additional
+        # forward arguments
+        # that are nd-tensors. It is assumed that the first dimension is
+        # the number of batches.
+        # dim -> (bsz * #steps x additional_forward_args[0].shape[1:], ...)
+        input_additional_args = (
+            _expand_additional_forward_args(additional_forward_args, n_steps)
+            if additional_forward_args is not None
+            else None
+        )
+        expanded_target = _expand_target(target, n_steps)
+
+        # grads: dim -> (bsz * #steps x inputs[0].shape[1:], ...)
+        grads = self.gradient_func(
+            forward_fn=self.forward_func,
+            inputs=scaled_features_tpl,
+            target_ind=expanded_target,
+            additional_forward_args=input_additional_args,
+        )
+
+        # flattening grads so that we can multilpy it with step-size
+        # calling contiguous to avoid `memory whole` problems
+        scaled_grads = [
+            grad.contiguous().view(n_steps, -1)
+            * torch.tensor(step_sizes).view(n_steps, 1).to(grad.device)
+            for grad in grads
+        ]
+
+        # aggregates across all steps for each tensor in the input tuple
+        # total_grads has the same dimensionality as inputs
+        total_grads = tuple(
+            _reshape_and_sum(
+                scaled_grad, n_steps, grad.shape[0] // n_steps, grad.shape[1:]
+            )
+            for (scaled_grad, grad) in zip(scaled_grads, grads)
+        )
+
+        # computes attribution for each tensor in input tuple
+        # attributions has the same dimensionality as inputs
+        if not self.multiplies_by_inputs:
+            attributions = total_grads
+        else:
+            attributions = tuple(
+                total_grad * (input - baseline)
+                for total_grad, input, baseline in zip(
+                    total_grads, inputs, baselines
+                )
+            )
+        return attributions
+
+    @staticmethod
+    def scale_features(
+        inputs: Tuple[Tensor, ...],
+        baselines: Tuple[Union[Tensor, int, float], ...],
+        alphas: List[float],
+    ) -> Tuple[Tensor]:
+        scaled_features_tpl = tuple(
+            torch.cat(
+                [
+                    torch.cat(
+                        [
+                            input[:, :-1, ...],
+                            (
+                                baseline
+                                + alpha * (input[:, -1, ...] - baseline)
+                            ).unsqueeze(1),
+                        ],
+                        dim=1,
+                    )
+                    for alpha in alphas
+                ],
+                dim=0,
+            ).requires_grad_()
+            for input, baseline in zip(inputs, baselines)
+        )
+
+        return scaled_features_tpl
 
     def compute_partial_attribution(
         self,
