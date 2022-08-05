35b1dd42f4b1511e21bae9348937f24a32897916
josephenguehard <josephenguehard@gmail.com>
2022-08-05 14:45:47 +0100

    Refactor retain

--- tint/attr/retain.py
+++ tint/attr/retain.py
@@ -1,4 +1,5 @@
 import copy
+
 import torch as th
 
 from captum.attr._utils.attribution import PerturbationAttribution
@@ -10,8 +11,9 @@ from captum._utils.common import (
 )
 from captum._utils.typing import TensorOrTupleOfTensorsGeneric, TargetType
 
-from pytorch_lightning import Trainer
+from pytorch_lightning import LightningDataModule, Trainer
 from torch.utils.data import DataLoader, TensorDataset
+from typing import Callable, Union
 
 from .models import Retain as RetainModel, RetainNet
 
@@ -24,17 +26,65 @@ class Retain(PerturbationAttribution):
         https://arxiv.org/pdf/1608.05745
     """
 
-    def __init__(self) -> None:
-        super().__init__(forward_func=None)
+    def __init__(
+        self,
+        forward_func: Callable = None,
+        retain: Union[RetainNet, RetainModel] = None,
+        datamodule: LightningDataModule = None,
+        features: th.Tensor = None,
+        labels: th.Tensor = None,
+        trainer: Trainer = None,
+        batch_size: int = 32,
+    ) -> None:
+        # If forward_func is not provided, try get retain model
+        if forward_func is None:
+            # If retain model is not provided,
+            # create it and train it
+            if retain is None:
+
+                # Create dataloader if not provided
+                dataloader = None
+                if datamodule is None:
+                    assert (
+                        features is not None
+                    ), "You must provide either a datamodule or features"
+                    assert (
+                        labels is not None
+                    ), "You must provide either a datamodule or labels"
+
+                    dataloader = DataLoader(
+                        TensorDataset(features, labels),
+                        batch_size=batch_size,
+                    )
+
+                # Init trainer if not provided
+                if trainer is None:
+                    trainer = Trainer(max_epochs=100)
+                else:
+                    trainer = copy.deepcopy(trainer)
+
+                # Create and fit model
+                retain = RetainNet(loss="cross_entropy")
+                trainer.fit(
+                    retain, train_dataloaders=dataloader, datamodule=datamodule
+                )
+
+                # Set to eval mode
+                retain.eval()
+
+            # Extract forward_func from model
+            if isinstance(retain, RetainModel):
+                forward_func = retain
+            else:
+                forward_func = retain.net
+
+        super().__init__(forward_func=forward_func)
 
     @log_usage()
     def attribute(
         self,
         inputs: TensorOrTupleOfTensorsGeneric,
         target: TargetType,
-        trainer: Trainer = None,
-        retain_net: RetainNet = None,
-        batch_size: int = 32,
     ) -> TensorOrTupleOfTensorsGeneric:
         """
         attribute method.
@@ -43,11 +93,6 @@ class Retain(PerturbationAttribution):
             inputs (tuple, th.Tensor): Input data.
             target (int, tuple, tensor, list): Output indices. Default to
                 ``None``
-            trainer (Trainer): Pytorch Lightning trainer. If ``None``, a
-                default trainer will be provided. Default to ``None``
-            retain_net (RetainNet): A Retain model. If ``None``, a default
-            model will be provided. Default to ``None``
-            batch_size (int): Batch size for Retain training. Default to 32
 
         Returns:
             (th.Tensor, tuple): Attributions.
@@ -57,12 +102,6 @@ class Retain(PerturbationAttribution):
         is_inputs_tuple = _is_tuple(inputs)
         inputs = _format_input(inputs)
 
-        # Init trainer if not provided
-        if trainer is None:
-            trainer = Trainer(max_epochs=100)
-        else:
-            trainer = copy.deepcopy(trainer)
-
         # Assert only one input, as the Retain only accepts one
         assert (
             len(inputs) == 1
@@ -71,27 +110,9 @@ class Retain(PerturbationAttribution):
         # Make target a tensor
         target = self._format_target(inputs, target)
 
-        # Init MaskNet if not provided
-        if retain_net is None:
-            retain_net = RetainNet(loss="cross_entropy")
-        else:
-            retain_net = copy.deepcopy(retain_net)
-
-        # Prepare data
-        dataloader = DataLoader(
-            TensorDataset(inputs[0], target), batch_size=batch_size
-        )
-
-        # Fit model
-        trainer.fit(retain_net, train_dataloaders=dataloader)
-
-        # Set model to eval mode
-        retain_net.eval()
-
         # Get attributions
         attributions = (
             self.representation(
-                model=retain_net.net,
                 inputs=inputs[0],
                 target=target,
             ),
@@ -99,9 +120,8 @@ class Retain(PerturbationAttribution):
 
         return _format_output(is_inputs_tuple, attributions)
 
-    @staticmethod
     def representation(
-        model: RetainModel,
+        self,
         inputs: th.Tensor,
         target: th.Tensor = None,
     ):
@@ -109,7 +129,6 @@ class Retain(PerturbationAttribution):
         Get representations based on a model, inputs and potentially targets.
 
         Args:
-            model (RetainModel): A Retain model.
             inputs (th.Tensor): Input data.
             target (th.Tensor): Targets. Default to ``None``
 
@@ -118,14 +137,14 @@ class Retain(PerturbationAttribution):
         """
         score = th.zeros(inputs.shape)
 
-        logit, alpha, beta = model(
+        logit, alpha, beta = self.forward_func(
             inputs, (th.ones((len(inputs),)) * inputs.shape[1]).long()
         )
-        w_emb = model.embedding[1].weight
+        w_emb = self.forward_func.embedding[1].weight
 
         for i in range(inputs.shape[2]):
             for t in range(inputs.shape[1]):
-                imp = model.output(
+                imp = self.forward_func.output(
                     beta[:, t, :] * w_emb[:, i].expand_as(beta[:, t, :])
                 )
                 if target is None:
