9076b7e48109a4c0d9aaf5ce154292ef92f3d6e9
josephenguehard <josephenguehard@gmail.com>
2022-08-16 11:06:20 +0200

    Fix clamp error

--- experiments/arma/main.py
+++ experiments/arma/main.py
@@ -61,7 +61,7 @@ def main(
             batch_size=50,
             additional_forward_args=(true_saliency,),
         )
-        attr["bayes_mask"] = _attr.clamp(0, 1)
+        attr["bayes_mask"] = _attr
 
     if "dyna_mask" in explainers:
         trainer = Trainer(max_epochs=1000, accelerator=accelerator, devices=1)
