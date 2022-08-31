046811ba2a0d595fa846d4ee889612b173b55838
josephenguehard <josephenguehard@gmail.com>
2022-08-31 15:32:23 +0200

    Add more docs

--- tint/metrics/white_box/information.py
+++ tint/metrics/white_box/information.py
@@ -28,6 +28,42 @@ def information(
     true_attributions: TensorOrTupleOfTensorsGeneric,
     normalize: bool = True,
 ) -> Tuple[float]:
+    """
+    Information measure of the attributions over the true_attributions.
+
+    This metric measures how much information there is in the attributions.
+    Higher is better.
+
+    Args:
+        attributions (tensor or tuple of tensors):
+            The attributions with respect to each input feature.
+            Attributions will always be
+            the same size as the provided inputs, with each value
+            providing the attribution of the corresponding input index.
+            If a single tensor is provided as inputs, a single float
+            is returned. If a tuple is provided for inputs, a tuple of
+            float is returned.
+        true_attributions (tensor or tuple of tensors):
+            True attributions to be used as a benchmark. Should be of
+            the same format as the attributions.
+        normalize (bool): Whether to normalize the attributions before
+            computing the metric or not. Default: True
+
+    Returns:
+        (float or tuple or floats): The aur metric.
+
+    References:
+        https://arxiv.org/pdf/2106.05303
+
+    Examples:
+        >>> import torch as th
+        >>> from tint.metrics.white_box import information
+        <BLANKLINE>
+        >>> attr = th.rand(8, 7, 5)
+        >>> true_attr = th.randint(2, (8, 7, 5))
+        <BLANKLINE>
+        >>> information_ = information(attr, true_attr)
+    """
     return _base_white_box_metric(
         metric=_information,
         attributions=attributions,
