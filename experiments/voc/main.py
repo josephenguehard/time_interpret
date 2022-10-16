8e166c9e83255b90925fcabe1d17e1eed0b655cf
josephenguehard <josephenguehard@gmail.com>
2022-10-16 17:07:23 +0100

    Fix dim error in extremal_mask

--- experiments/voc/main.py
+++ experiments/voc/main.py
@@ -298,7 +298,7 @@ def main(
         mask = ExtremalMaskNet(
             forward_func=resnet,
             comp_loss=True,
-            model=CNN([1, 1], kernel_size=3, padding=1, flatten=False),
+            model=CNN([3, 3], kernel_size=3, padding=1, flatten=False),
             optim="adam",
             lr=0.01,
         )
