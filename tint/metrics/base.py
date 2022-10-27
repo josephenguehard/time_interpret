455b1faec2eee9cec35f4a1530d567777b842f1b
josephenguehard <josephenguehard@gmail.com>
2022-10-27 12:39:34 +0100

    Add n_samples_batch_size to avoid oom errors

--- tint/metrics/base.py
+++ tint/metrics/base.py
@@ -33,6 +33,7 @@ def _base_metric(
     additional_forward_args: Any = None,
     target: TargetType = None,
     n_samples: int = 1,
+    n_samples_batch_size: int = None,
     stdevs: Union[float, Tuple[float, ...]] = 0.0,
     draw_baseline_from_distrib: bool = False,
     topk: float = 0.2,
@@ -43,11 +44,12 @@ def _base_metric(
     classification: bool = True,
     **kwargs,
 ) -> float:
-    # Format inputs and add noise
+    # Format data
     inputs = _format_tensor_into_tuples(inputs)  # type: ignore
-    inputs = add_noise_to_inputs(inputs, stdevs, n_samples)
-
-    # Format, validate and expand baselines
+    additional_forward_args = _format_additional_forward_args(
+        additional_forward_args
+    )
+    attributions = _format_tensor_into_tuples(attributions)  # type: ignore
     if baselines is not None:
         baselines = _format_baseline(
             baselines, cast(Tuple[Tensor, ...], inputs)
@@ -57,113 +59,136 @@ def _base_metric(
             baselines,
             draw_baseline_from_distrib=draw_baseline_from_distrib,
         )
-        baselines = _expand_baselines(
-            inputs,
-            baselines,
-            n_samples,
-            draw_baseline_from_distrib,
-        )
 
-    # Format and expand additional args
-    additional_forward_args = _format_additional_forward_args(
-        additional_forward_args
-    )
-    additional_forward_args = _expand_additional_forward_args(
-        additional_forward_args, n_samples
-    )
+    # Set n_samples_batch_size to n_samples if not provided
+    if n_samples_batch_size is None:
+        n_samples_batch_size = n_samples
 
-    # Expand target
-    target = _expand_target(target, n_samples)
+    # Loop over batches
+    out = list()
+    weights = list() if weight_fn else None
+    for i in range(n_samples // n_samples_batch_size + 1):
+        if n_samples - i * n_samples_batch_size >= n_samples_batch_size:
+            n = n_samples_batch_size
+        elif n_samples - i * n_samples_batch_size > 0:
+            n = n_samples - i * n_samples_batch_size
+        else:
+            break
 
-    # Format and expand attributions
-    attributions = _format_tensor_into_tuples(attributions)  # type: ignore
-    attributions = _expand_additional_forward_args(attributions, n_samples)
+        # Add noise to inputs and expand
+        _inputs = add_noise_to_inputs(inputs, stdevs, n)
 
-    # Validate topk
-    assert 0 < topk < 1, "topk must be a float between 0 and 1"
-
-    # Reverse topk is not largest to select the non topk
-    if not largest:
-        topk = 1.0 - topk
-
-    # Clone inputs
-    inputs_pert = tuple(inp.detach().clone() for inp in inputs)
-
-    # Get topk indices for each element in the batch
-    # It is assumed that the batch is on the first dimension
-    topk_indices = tuple(
-        torch.topk(
-            attr.reshape(len(attr), -1),
-            int(attr.reshape(len(attr), -1).shape[-1] * topk),
-            sorted=False,
-            largest=largest,
-        ).indices.to(attr.device)
-        for attr in attributions
-    )
-
-    # Set topk indices to inputs device
-    topk_indices = tuple(
-        topk.to(input.device) for topk, input in zip(topk_indices, inputs)
-    )
-
-    # Replace topk values with baseline
-    if baselines is None:
-        inputs_pert = tuple(
-            inp.reshape(len(inp), -1)
-            .scatter(-1, topk_idx, 0)
-            .reshape(inp.shape)
-            for inp, topk_idx in zip(inputs_pert, topk_indices)
-        )
-    else:
-        baselines = tuple(
-            baseline
-            if isinstance(baseline, (int, float))
-            else baseline.reshape(len(baseline), -1).gather(-1, topk_idx)
-            for baseline, topk_idx in zip(baselines, topk_indices)
-        )
-        inputs_pert = tuple(
-            inp.reshape(len(inp), -1)
-            .scatter(-1, topk_idx, baseline)
-            .reshape(inp.shape)
-            for inp, baseline, topk_idx in zip(
-                inputs_pert, baselines, topk_indices
+        # Expand baselines
+        _baselines = None
+        if baselines is not None:
+            _baselines = _expand_baselines(
+                _inputs,
+                baselines,
+                n,
+                draw_baseline_from_distrib,
             )
+
+        # Expand additional args
+        _additional_forward_args = _expand_additional_forward_args(
+            additional_forward_args, n
         )
 
-    # Get weights if provided
-    weights = None
+        # Expand target
+        _target = _expand_target(target, n)
+
+        # Format and expand attributions
+        _attributions = _expand_additional_forward_args(attributions, n)
+
+        # Validate topk
+        assert 0 < topk < 1, "topk must be a float between 0 and 1"
+
+        # Reverse topk is not largest to select the non topk
+        if not largest:
+            topk = 1.0 - topk
+
+        # Clone inputs
+        inputs_pert = tuple(inp.detach().clone() for inp in _inputs)
+
+        # Get topk indices for each element in the batch
+        # It is assumed that the batch is on the first dimension
+        topk_indices = tuple(
+            torch.topk(
+                attr.reshape(len(attr), -1),
+                int(attr.reshape(len(attr), -1).shape[-1] * topk),
+                sorted=False,
+                largest=largest,
+            ).indices.to(attr.device)
+            for attr in _attributions
+        )
+
+        # Set topk indices to inputs device
+        topk_indices = tuple(
+            topk.to(input.device) for topk, input in zip(topk_indices, _inputs)
+        )
+
+        # Replace topk values with baseline
+        if _baselines is None:
+            inputs_pert = tuple(
+                inp.reshape(len(inp), -1)
+                .scatter(-1, topk_idx, 0)
+                .reshape(inp.shape)
+                for inp, topk_idx in zip(inputs_pert, topk_indices)
+            )
+        else:
+            _baselines = tuple(
+                baseline
+                if isinstance(baseline, (int, float))
+                else baseline.reshape(len(baseline), -1).gather(-1, topk_idx)
+                for baseline, topk_idx in zip(_baselines, topk_indices)
+            )
+            inputs_pert = tuple(
+                inp.reshape(len(inp), -1)
+                .scatter(-1, topk_idx, baseline)
+                .reshape(inp.shape)
+                for inp, baseline, topk_idx in zip(
+                    inputs_pert, _baselines, topk_indices
+                )
+            )
+
+        # Get weights if provided
+        if weight_fn:
+            weights.append(weight_fn(_inputs, inputs_pert))
+
+        # Get original predictions
+        logits_original = _run_forward(
+            forward_func=forward_func,
+            inputs=_inputs,
+            target=None,
+            additional_forward_args=_additional_forward_args,
+        )
+        prob_original = logits_original.softmax(-1)
+
+        # Get predictions for perturbed inputs
+        logits_pert = _run_forward(
+            forward_func=forward_func,
+            inputs=inputs_pert,
+            target=None,
+            additional_forward_args=_additional_forward_args,
+        )
+        prob_pert = logits_pert.softmax(-1)
+
+        # Get target as original predictions if not provided
+        if _target is None and classification:
+            _target = logits_original.argmax(-1)
+
+        if classification:
+            out.append(metric(prob_original, prob_pert, _target, **kwargs))
+        else:
+            out.append(metric(logits_original, logits_pert, _target, **kwargs))
+
+    # Concat results
+    out = torch.cat(out)
+
+    # If weight_fn provided return a weighted average
     if weight_fn:
-        weights = weight_fn(inputs, inputs_pert)
-
-    # Get original predictions
-    logits_original = _run_forward(
-        forward_func=forward_func,
-        inputs=inputs,
-        target=None,
-        additional_forward_args=additional_forward_args,
-    )
-    prob_original = logits_original.softmax(-1)
-
-    # Get predictions for perturbed inputs
-    logits_pert = _run_forward(
-        forward_func=forward_func,
-        inputs=inputs_pert,
-        target=None,
-        additional_forward_args=additional_forward_args,
-    )
-    prob_pert = logits_pert.softmax(-1)
-
-    # Get target as original predictions if not provided
-    if target is None and classification:
-        target = logits_original.argmax(-1)
-
-    if classification:
-        out = metric(prob_original, prob_pert, target, **kwargs)
-    else:
-        out = metric(logits_original, logits_pert, target, **kwargs)
-
-    if weight_fn:
+        weights = torch.cat(weights)
         weights = weights.to(out.device)
         return (out * weights).sum().item() / weights.sum().item()
 
+    # Otherwise return the average
     return out.mean().item()
