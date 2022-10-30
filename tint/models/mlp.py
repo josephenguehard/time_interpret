a094a6508955df3a01c36d7b45b00d4812d37aa4
josephenguehard <josephenguehard@gmail.com>
2022-10-30 20:19:45 +0000

    Change beta in softplus act in mlp

--- tint/models/mlp.py
+++ tint/models/mlp.py
@@ -19,7 +19,7 @@ ACTIVATIONS = {
     "relu6": nn.ReLU6(),
     "sigmoid": nn.Sigmoid(),
     "softmax": nn.Softmax(dim=-1),
-    "softplus": nn.Softplus(),
+    "softplus": nn.Softplus(beta=5),
     "softsign": nn.Softsign(),
     "tanh": nn.Tanh(),
     "tanhshrink": nn.Tanhshrink(),
