fc2f52deca4bec2743404ce65331849bd0b4e8df
josephenguehard <josephenguehard@gmail.com>
2022-11-08 19:27:15 +0000

    Add functional replacement and small refactoring

--- tint/attr/non_linearities_tunnel.py
+++ tint/attr/non_linearities_tunnel.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch.nn.functional as F
 
 from captum.attr._utils.attribution import Attribution, GradientAttribution
 from captum.log import log_usage
@@ -16,20 +17,24 @@ from typing import Any, Tuple, Union, cast
 
 class NonLinearitiesTunnel(Attribution):
     r"""
-    Replace non linearities (or any module) with other before running
+    Replace non linearities (or any module) with others before running
     an attribution method. This tunnel is originally intended to
     replace ReLU activations with Softplus to smooth the explanations.
 
     .. warning::
-        This module only replace ``layers`` such as `nn.ReLU()`,
-        not functional methods such as `F.relu` or `F.relu_`
+        This method will break if the forward_func contains functional
+        non linearities with additional arguments that need to be replaced.
+        For instance, replacing ``F.softmax(x, dim=-1)`` is not possible due
+        to the presence of the extra argument ``dim``.
 
-    .. note::
-        This module only works when ``forward_func`` is a PyTorch nn.Module
+    .. hint::
+        In order to replace any layer, a nn.Module must be passed as
+        forward_func. In particular, passing ``model.forward`` will result
+        in not replacing any layer in ``model``.
 
     Args:
-        attribution_method (Attribution): An instance of any attribution algorithm
-            of type `Attribution`. E.g. Integrated Gradients,
+        attribution_method (Attribution): An instance of any attribution
+            algorithm of type `Attribution`. E.g. Integrated Gradients,
             Conductance or Saliency.
 
     References:
@@ -74,9 +79,7 @@ class NonLinearitiesTunnel(Attribution):
         self,
         inputs: Union[Tensor, Tuple[Tensor, ...]],
         to_replace: Union[nn.Module, Tuple[nn.Module, ...]] = nn.ReLU(),
-        replace_by: Union[nn.Module, Tuple[nn.Module, ...]] = nn.Softplus(
-            beta=10
-        ),
+        replace_with: Union[nn.Module, Tuple[nn.Module, ...]] = nn.Softplus(),
         **kwargs: Any,
     ) -> Union[
         Union[
@@ -100,14 +103,15 @@ class NonLinearitiesTunnel(Attribution):
                 dimension 1 corresponds to the time dimension, and if
                 multiple input tensors are provided, the examples must
                 be aligned appropriately.
-            to_replace (nn.Module, list, optional): List of non linearities
+            to_replace (nn.Module, tuple, optional): Non linearities
                 to be  replaced. The linearities of type listed here will be
                 replaced by ``replaced_by`` non linearities before running
-                the attribution method.
+                the attribution method. This can be an instance or a class.
+                If a class is passed, default attributes are used.
                 Default: nn.ReLU()
-            replace_by (nn.Module, list, optional): List of non linearities
-                to replace non linearities listed in ``to_replace``.
-                Default: nn.Softplus(beta=10)
+            replace_with (nn.Module, tuple, optional): Non linearities
+                to replace the ones listed in ``to_replace``.
+                Default: nn.Softplus()
             **kwargs: (Any, optional): Contains a list of arguments that are
                 passed  to `attribution_method` attribution algorithm.
                 Any additional arguments that should be used for the
@@ -143,41 +147,60 @@ class NonLinearitiesTunnel(Attribution):
             and kwargs["return_convergence_delta"]
         )
 
-        # forward_func must be a PyTorch module
-        assert isinstance(
-            self.attribution_method.forward_func, nn.Module
-        ), "forward_func must be a PyTorch module with this method."
+        _replaced_layers_tpl = None
+        _replaced_functions = dict()
+        try:
+            # Replace layers using to_replace and replace_with
+            if not isinstance(to_replace, tuple):
+                to_replace = (to_replace,)
+            if not isinstance(replace_with, tuple):
+                replace_with = (replace_with,)
 
-        # Replace layers using to_replace and replaced_by
-        if not isinstance(to_replace, tuple):
-            to_replace = (to_replace,)
-        if not isinstance(replace_by, tuple):
-            replace_by = (replace_by,)
+            if isinstance(self.attribution_method.forward_func, nn.Module):
+                _replaced_layers_tpl = tuple(
+                    replace_layers(
+                        self.attribution_method.forward_func, old, new
+                    )
+                    for old, new in zip(to_replace, replace_with)
+                )
 
-        _replaced_layers_tpl = tuple(
-            replace_layers(self.attribution_method.forward_func, old, new)
-            for old, new in zip(to_replace, replace_by)
-        )
+            # Replace functional using to_replace and replace_with
+            for old, new in zip(to_replace, replace_with):
+                name, _ = get_functional(old)
+                _, func = get_functional(new)
+                _replaced_functions[name] = getattr(F, name)
+                setattr(F, name, func)
 
-        # Get attributions
-        attributions = self.attribution_method.attribute.__wrapped__(
-            self.attribution_method,  # self
-            inputs if is_inputs_tuple else inputs[0],
-            **kwargs,
-        )
+            # Get attributions
+            attributions = self.attribution_method.attribute.__wrapped__(
+                self.attribution_method,  # self
+                inputs if is_inputs_tuple else inputs[0],
+                **kwargs,
+            )
 
-        # Get delta if required
-        delta = None
-        if self.is_delta_supported and return_convergence_delta:
-            attributions, delta = attributions
+            # Get delta if required
+            delta = None
+            if self.is_delta_supported and return_convergence_delta:
+                attributions, delta = attributions
 
-        # Format attributions
-        is_attrib_tuple = _is_tuple(attributions)
-        attributions = _format_tensor_into_tuples(attributions)
+            # Format attributions
+            is_attrib_tuple = _is_tuple(attributions)
+            attributions = _format_tensor_into_tuples(attributions)
 
-        # Restore non linearities
-        for layer in _replaced_layers_tpl:
-            reverse_replace_layers(self.attribution_method.forward_func, layer)
+        # Even if any error is raised, restore layers and functions
+        # before raising
+        finally:
+            # Restore layers
+            if _replaced_layers_tpl is not None:
+                for layer in _replaced_layers_tpl:
+                    reverse_replace_layers(
+                        self.attribution_method.forward_func,
+                        layer,
+                    )
+
+            # Restore functions
+            for name, func in _replaced_functions.items():
+                setattr(F, name, func)
 
         return self._apply_checks_and_return_attributions(
             attributions,
@@ -224,24 +247,100 @@ def replace_layers(model, old, new):
     References:
         https://discuss.pytorch.org/t/how-to-modify-a-pretrained-model/60509/12
     """
-    replaced_names = dict()
+    replaced_layers = dict()
     for n, module in model.named_children():
         if len(list(module.children())) > 0:
-            replaced_names[n] = replace_layers(module, old, new)
+            replaced_layers[n] = replace_layers(module, old, new)
 
         if isinstance(module, old if type(old) == type else type(old)):
-            replaced_names[n] = getattr(model, n)
+            replaced_layers[n] = getattr(model, n)
             setattr(model, n, new)
-    return replaced_names
+    return replaced_layers
 
 
-def reverse_replace_layers(model, replaced_names):
+def reverse_replace_layers(model, replaced_layers):
     """
-    Reverse the layer replacement using the ``replaced_names``
+    Reverse the layer replacement using the ``replaced_layers``
     dictionary created when running ``replace_layers``.
     """
-    for k, v in replaced_names.items():
+    for k, v in replaced_layers.items():
         if isinstance(v, dict):
             reverse_replace_layers(getattr(model, k), v)
         else:
             setattr(model, k, v)
+
+
+def get_functional(module):
+    """
+    Map a nn Non linearity to a corresponding function.
+    It returns the name of the function to be replaced and the function
+    to replace it with.
+    """
+    # Get default instance if ony type provided
+    if type(module) == type:
+        module = module()
+
+    assert isinstance(module, nn.Module), "You must provide a PyTorch Module."
+
+    if isinstance(module, nn.Threshold):
+        threshold = module.threshold
+        value = module.value
+        inplace = module.inplace
+        if inplace:
+            return "threshold_", lambda x: F.threshold_(x, threshold, value)
+        return "threshold", lambda x: F.threshold(x, threshold, value)
+
+    if isinstance(module, nn.ReLU):
+        inplace = module.inplace
+        if inplace:
+            return "relu_", F.relu_
+        return "relu", F.relu
+
+    if isinstance(module, nn.ReLU6):
+        inplace = module.inplace
+        return "relu6", lambda x: F.relu6(x, inplace)
+
+    if isinstance(module, nn.ELU):
+        alpha = module.alpha
+        inplace = module.inplace
+        if inplace:
+            return "elu_", lambda x: F.elu_(x, alpha)
+        return "elu", lambda x: F.elu(x, alpha)
+
+    if isinstance(module, nn.CELU):
+        alpha = module.alpha
+        inplace = module.inplace
+        if inplace:
+            return "celu_", lambda x: F.celu_(x, alpha)
+        return "celu", lambda x: F.celu(x, alpha)
+
+    if isinstance(module, nn.LeakyReLU):
+        negative_slope = module.negative_slope
+        inplace = module.inplace
+        if inplace:
+            return "leaky_relu_", lambda x: F.leaky_relu_(x, negative_slope)
+        return "leaky_relu", lambda x: F.leaky_relu(x, negative_slope)
+
+    if isinstance(module, nn.Softplus):
+        beta = module.beta
+        threshold = module.threshold
+        return "softplus", lambda x: F.softplus(x, beta, threshold)
+
+    if isinstance(module, nn.Softmax):
+        dim = module.dim
+        return "softmax", lambda x: F.softmax(x, dim=dim)
+
+    if isinstance(module, nn.LogSoftmax):
+        dim = module.dim
+        return "log_softmax", lambda x: F.log_softmax(x, dim=dim)
+
+    if isinstance(module, nn.Sigmoid):
+        return "sigmoid", F.sigmoid
+
+    if isinstance(module, nn.Tanh):
+        return "tanh", F.tanh
+
+    if isinstance(module, nn.Tanhshrink):
+        return "tanhshrink", F.tanhshrink
+
+    return None
