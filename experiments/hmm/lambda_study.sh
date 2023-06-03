79aee892a28683dc04d4cf87ffc2ce17d1335925
josephenguehard <josephenguehard@gmail.com>
2023-06-03 13:23:57 +0100

    Support older bash versions in exp

--- experiments/hmm/lambda_study.sh
+++ experiments/hmm/lambda_study.sh
@@ -29,12 +29,24 @@ do
     do
       python main.py --explainers extremal_mask --device "$device" --fold "$fold" --seed "$seed" --lambda-1 "$lambda_1" --lambda-2 "$lambda_2" --output-file lambda_study.csv &
 
-      # allow to execute up to $processes jobs in parallel
-      if [[ $(jobs -r -p | wc -l) -ge $processes ]]
+      # Support lower versions
+      if ((BASH_VERSINFO[0] >= 4)) && ((BASH_VERSINFO[1] >= 3))
       then
-        # now there are $processes jobs already running, so wait here for any job
-        # to be finished so there is a place to start next one.
-        wait -n
+        # allow to execute up to $processes jobs in parallel
+        if [[ $(jobs -r -p | wc -l) -ge $processes ]]
+        then
+          # now there are $processes jobs already running, so wait here for any job
+          # to be finished so there is a place to start next one.
+          wait -n
+        fi
+      else
+        # allow to execute up to $processes jobs in parallel
+        while [[ $(jobs -r -p | wc -l) -ge $processes ]]
+        do
+          # now there are $processes jobs already running, so wait here for any job
+          # to be finished so there is a place to start next one.
+          sleep 1
+        done
       fi
 
     done
