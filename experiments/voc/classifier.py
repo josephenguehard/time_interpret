958d3c482eaee9d0a86d549c5a8934e5e8adeae0
josephenguehard <josephenguehard@gmail.com>
2023-06-25 21:42:41 +0200

    Refacfor voc exp

--- experiments/voc/classifier.py
+++ experiments/voc/classifier.py
@@ -8,7 +8,9 @@ class VocClassifier(nn.Module):
     def __init__(self):
         super(VocClassifier, self).__init__()
         self.resnet = resnet18(pretrained=True)
+        self.linear = nn.Linear(1000, 20)
 
     def forward(self, x):
         x = self.resnet(x)
+        x = self.linear(x)
         return F.log_softmax(x, dim=-1)
